f8219d001538fbb169279155a3e62222067ac561
licetrosales <licetrosales@gmail.com>
2023-03-11 16:41:46 +0100

    Add class with address info

---
+++ src/main/java/Adresse.java
@@ -0,0 +1,13 @@
+public class Adresse {
+    String strasse;
+    int hausnummer;
+    String plz;
+    String ort;
+    public Adresse(String strasse, int hausnummer, String plz, String ort) {
+        this.strasse = strasse;
+        this.hausnummer = hausnummer;
+        this.plz = plz;
+        this.ort = ort;
+    }
+
+}
