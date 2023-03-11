d775a56cbee234e97a8ec7aeca023c7a7eed4c4d
licetrosales <licetrosales@gmail.com>
2023-03-11 17:48:10 +0100

    Declare class Kunde as subclass of class Person

--- src/main/java/Kunde.java
+++ src/main/java/Kunde.java
@@ -1,13 +1,5 @@
-public class Kunde {
-    String vorname;
-    String nachname;
-    Adresse adresse;
-    public String ganzerName() {
-        return vorname + " " + nachname;
-    }
-    public Kunde(String vn, String nn, String str, int nummer, String p, String o){
-        vorname = vn;
-        nachname = nn;
-        adresse = new Adresse(str, nummer, p, o);
+public class Kunde extends Person {
+    public Kunde(String vorname, String nachname, String strasse, int hausnummer, String plz, String ort){
+        super(vorname, nachname, strasse, hausnummer, plz, ort);
     }
 }
