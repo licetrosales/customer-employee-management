4feccc073322d0230f256ece297f8c433285c157
licetrosales <licetrosales@gmail.com>
2023-03-11 16:54:31 +0100

    Insert Adresse class in Kunde class

--- src/main/java/Kunde.java
+++ src/main/java/Kunde.java
@@ -1,20 +1,15 @@
 public class Kunde {
     String vorname;
     String nachname;
-    String strasse;
-    int hausnummer;
-    String plz;
-    String ort;
+    Adresse adresse;
     public String ganzerName() {
         return vorname + " " + nachname;
     }
     public Kunde(String vn, String nn, String str, int nummer, String p, String o){
         vorname = vn;
         nachname = nn;
-        strasse = str;
-        hausnummer = nummer;
-        plz = p;
-        ort = o;
+        adresse = new Adresse(str, nummer, p, o);
+
     }
 
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
             Kunde k1 = new Kunde ("Mike", "Mustermann", "Musterstrasse", 7, "01234", "Musterstadt");
-            Kunde k2 = new Kunde ("Mary", "Musterfrau", "Musterstrasse", 7, "01234", "Musterstadt");
+            Kunde k2 = new Kunde ("Mary", "Musterfrau", "Musterstrasse", 9, "01234", "Musterstadt");
             System.out.println(k1.vorname);
             System.out.println(k1.ganzerName());
+        System.out.println(k2.adresse.hausnummer);
 
     }
 }
