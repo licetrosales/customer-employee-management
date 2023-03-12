663cecc8d225ee1a5504fa2785da59ddc31b8c66
licetrosales <licetrosales@gmail.com>
2023-03-12 18:24:15 +0100

    Add List of objects

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,3 +2,9 @@
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/Nutzereingabe.java,1/8/182478070d239a84ede4c525c32f678d390566bc
+w
+G.idea/sonarlint/issuestore/a/5/a5cc2925ca8258af241be7e5b0381edf30266302,e/d/ed67eb178d523a4b3f117dcbe0eda5196367ceec
--- src/main/java/Personenverwaltung.java
+++ src/main/java/Personenverwaltung.java
@@ -1,42 +1,56 @@
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Personenverwaltung {
     // Einlesen der Personendaten
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Neue Person erfassen");
-        System.out.println("Bitte geben Sie den Vornamen der Person ein:");
-        String vorname = scanner.next();
+        List<Person> personen = new LinkedList<>();
+        boolean erfassen = true;
 
-        System.out.println("Bitte geben Sie den Nachnamen der Person ein:");
-        String nachname = scanner.next();
+        while ( erfassen) {
 
-        System.out.println("Bitte geben Sie die Straße an, in der die Person wohnt:");
-        String strasse = scanner.next();
+            System.out.println("Neue Person erfassen");
+            System.out.println("Bitte geben Sie den Vornamen der Person ein:");
+            String vorname = scanner.next();
 
-        System.out.println("Bitte geben Sie die Hausnummer an, in der die Person wohnt:");
-        int hausnummer = Integer.valueOf(scanner.next());
+            System.out.println("Bitte geben Sie den Nachnamen der Person ein:");
+            String nachname = scanner.next();
 
-        System.out.println("Bitte geben Sie die PLZ ein, an der die Person wohnt:");
-        String plz = scanner.next();
+            System.out.println("Bitte geben Sie die Straße an, in der die Person wohnt:");
+            String strasse = scanner.next();
 
-        System.out.println("Bitte geben Sie den Ort ein, an dem die Person wohnt:");
-        String ort = scanner.next();
+            System.out.println("Bitte geben Sie die Hausnummer an, in der die Person wohnt:");
+            int hausnummer = Integer.valueOf(scanner.next());
 
-        System.out.println("Ist die Person ein Mitarbeiter? ja/ nein");
-        String mitarbeiter = scanner.next();
+            System.out.println("Bitte geben Sie die PLZ ein, an der die Person wohnt:");
+            String plz = scanner.next();
 
-        if(mitarbeiter.equals("ja")){
-            System.out.println("Bitte geben Sie das Gehalt der Person ein:");
-            double gehalt = Double.valueOf(scanner.next());
-            Mitarbeiter m = new Mitarbeiter(vorname, nachname, strasse, hausnummer, plz, ort, gehalt);
-            System.out.println(m.ganzerName());
-        } else {
-            Kunde k = new Kunde(vorname, nachname, strasse, hausnummer, plz,ort);
-            System.out.println(k.ganzerName());
+            System.out.println("Bitte geben Sie den Ort ein, an dem die Person wohnt:");
+            String ort = scanner.next();
+
+            System.out.println("Ist die Person ein Mitarbeiter? ja/ nein");
+            String mitarbeiter = scanner.next();
+
+            if (mitarbeiter.equals("ja")) {
+                System.out.println("Bitte geben Sie das Gehalt der Person ein:");
+                double gehalt = Double.valueOf(scanner.next());
+                Mitarbeiter m = new Mitarbeiter(vorname, nachname, strasse, hausnummer, plz, ort, gehalt);
+                personen.add(m);
+                System.out.println(m.ganzerName());
+            } else {
+                Kunde k = new Kunde(vorname, nachname, strasse, hausnummer, plz, ort);
+                personen.add(k);
+                System.out.println(k.ganzerName());
+            }
+
+            System.out.println("Datenerfassung abschlossen. Möchten Sie fortfahren? ja/ nein");
+            String fortfahren = scanner.next();
+            if(fortfahren.equals("nein")){
+                erfassen = false;
+            }
         }
-
-        System.out.println("Datenerfassung abschlossen.");
     }
 
 }
