036159e22a58aa040cc22569397da41b070a281f
licetrosales <licetrosales@gmail.com>
2023-03-15 18:30:07 +0100

    Read array-attribut personen

--- src/main/java/JsonParser.java
+++ src/main/java/JsonParser.java
@@ -13,7 +13,7 @@ public class JsonParser {
         try{
             String text = new String(Files.readAllBytes(Paths.get("personen.json")), StandardCharsets.UTF_8);
             JSONObject json = new JSONObject(text);
-
+            JSONArray personen = json.getJSONArray("personen");
         } catch (IOException e){
             e.printStackTrace();
         }
