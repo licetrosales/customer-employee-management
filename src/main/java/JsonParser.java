82ac3d916c97541d3a935638b6df4fed69289a6e
licetrosales <licetrosales@gmail.com>
2023-03-15 18:57:22 +0100

    Read all lastnames

--- src/main/java/JsonParser.java
+++ src/main/java/JsonParser.java
@@ -11,9 +11,14 @@ import java.nio.file.Paths;
 public class JsonParser {
     public static void main(String[] args) {
         try{
-            String text = new String(Files.readAllBytes(Paths.get("personen.json")), StandardCharsets.UTF_8);
+            String text = new String(Files.readAllBytes(Paths.get("/personen.json")), StandardCharsets.UTF_8);
             JSONObject json = new JSONObject(text);
             JSONArray personen = json.getJSONArray("personen");
+            for(int i=0; i< personen.length();i++){
+                JSONObject person = personen.getJSONObject(i);
+                String nachname = person.getString("nachname");
+                System.out.println(nachname);
+            }
         } catch (IOException e){
             e.printStackTrace();
         }
