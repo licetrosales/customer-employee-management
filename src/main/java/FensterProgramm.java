0beeb43023cc241dc31ff2d8f6a4ae7ce7aa5876
licetrosales <licetrosales@gmail.com>
2023-03-20 21:48:10 +0100

    Set text appearance

--- src/main/java/FensterProgramm.java
+++ src/main/java/FensterProgramm.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FensterProgramm {
     public static void main(String[] args) {
         JFrame f = new JFrame();
         f.setSize(400, 500);
         f.setVisible(true);
+
         JTextArea textArea = new JTextArea();
+        textArea.setBackground(Color.BLUE);
+        textArea.setForeground(Color.WHITE);
+        textArea.setFont(new Font("Serif", Font.ITALIC, 16));
+
         f.add(textArea);
         f.setVisible(true);
+
+
 
     }
 }
